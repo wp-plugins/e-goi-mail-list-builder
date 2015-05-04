c01e2e4dcd9459789e13d04fbe08cac5130ea410
indot <indot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 18:11:30 +0000

    updated version
    git-svn-id: https://plugins.svn.wordpress.org/e-goi-mail-list-builder/trunk@1153147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- egoi-mail-list-builder.php
+++ egoi-mail-list-builder.php
@@ -29,7 +29,7 @@ License: GPLv2 or later
 /**
  * Define some useful constants
 **/
-define('EGOI_MAIL_LIST_BUILDER_VERSION', '1.0.5');
+define('EGOI_MAIL_LIST_BUILDER_VERSION', '1.0.6');
 define('EGOI_MAIL_LIST_BUILDER_DIR', plugin_dir_path(__FILE__));
 define('EGOI_MAIL_LIST_BUILDER_URL', plugin_dir_url(__FILE__));
 define('EGOI_MAIL_LIST_BUILDER_PLUGIN_KEY', 'ea5199d064c05237745156d5e4b82ef2');
